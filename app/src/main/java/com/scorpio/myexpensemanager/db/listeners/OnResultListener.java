302faaa107cf695b101489aa5b48fb59c56906b6
undriku <undriku@gmail.com>
2018-02-26 23:26:55 +0530

    Added code to support the company db

---
+++ app/src/main/java/com/scorpio/myexpensemanager/db/listeners/OnResultListener.java
@@ -0,0 +1,9 @@
+package com.scorpio.myexpensemanager.db.listeners;
+
+/**
+ * Created by User on 24-02-2018.
+ */
+
+public interface OnResultListener {
+    public void onResult(Long... results);
+}
